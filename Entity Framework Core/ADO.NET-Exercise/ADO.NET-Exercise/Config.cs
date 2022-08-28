0d5408c89a1a51cc6d050ba107f47e4802f437fd
Daniel Stefanov <97540472+ludsmotika@users.noreply.github.com>
2022-08-28 09:19:07 +0300

    Add files via upload

---
+++ Entity Framework Core/ADO.NET-Exercise/ADO.NET-Exercise/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.NET_Exercise
+{
+    public static class Config
+    {
+        public const string ConfigPath = @"Server=DESKTOP-FVGCI9R\SQLEXPRESS; DATABASE=MinionsDB; Integrated Security=true;";
+    }
+}
